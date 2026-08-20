2551d10fd8d01fdc3e895539b33220535e1b9351
rpraveshmandal-collab <rpraveshmandal@gmail.com>
2026-08-20 08:15:18 -0700

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -3,7 +3,7 @@
 // 2) Put your Project URL and anon/publishable key below.
 // 3) Run the SQL from SUPABASE_SETUP.md.
 window.RAID_CLOUD = {
-  url: 'https://YOUR_PROJECT.supabase.co',
-  anonKey: 'YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY',
+  url: 'https://ffcagenptflbekjpdckr.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmY2FnZW5wdGZsYmVranBkY2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjMxMDYsImV4cCI6MjEwMjc5OTEwNn0.YxVA15PyiT4tYJ95Gz_TcAOl11HgA6qDekcKlw7hQH8',
   table: 'raid_entries'
 };
