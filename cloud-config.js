739f28d8f24477a0411d2a577507f36eecbbe6b7
rpraveshmandal-collab <rpraveshmandal@gmail.com>
2026-08-22 08:22:03 -0700

    Delete cloud-config.js

--- cloud-config.js
+++
@@ -1,9 +0,0 @@
-// Cloud database configuration.
-// 1) Create a Supabase project.
-// 2) Put your Project URL and anon/publishable key below.
-// 3) Run the SQL from SUPABASE_SETUP.md.
-window.RAID_CLOUD = {
-  url: 'https://ffcagenptflbekjpdckr.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmY2FnZW5wdGZsYmVranBkY2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjMxMDYsImV4cCI6MjEwMjc5OTEwNn0.YxVA15PyiT4tYJ95Gz_TcAOl11HgA6qDekcKlw7hQH8',
-  table: 'raid_entries'
-};
